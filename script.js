faf012e5a93f28f36db82793bda340a79c9ac161
Ana-web-star <balanana.ic@gmail.com>
2025-04-18 20:17:53 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,3 +10,69 @@ function openReviews()
 {
     window.location.href = "reviews.html";
 }
+
+function openHistory()
+{
+    window.location.href= "F1, a short history.html";
+}
+function openTutorialF1()
+{
+    window.location.href="Everything, everywhere F1.html";
+}
+function openRules()
+{
+    window.location.href="The main sport's rules.html";
+}
+function openPoints()
+{
+    window.location.href="The scoring system.html";
+}
+function openTires()
+{
+    window.location.href="Tires.html";
+}
+
+function openTutorialF11()
+{
+    window.location.href="Get to know the drivers & the teams.html";
+}
+function openMcLaren()
+{
+    window.location.href="McLaren.html";
+}
+function openFerrari()
+{
+    window.location.href="Ferrari.html";
+}
+function openRedBull()
+{
+    window.location.href="RedBull.html";
+}
+function openMercedes()
+{
+    window.location.href="Mercedes.html";
+}
+function openAstonMartin()
+{
+    window.location.href="AstonMartin.html";
+}
+function openAlpine()
+{
+    window.location.href="Alpine.html";
+}
+function openHaas()
+{
+    window.location.href="Haas.html";
+}
+function openRacingBulls()
+{
+    window.location.href="RacingBulls.html";
+}
+function openWilliams()
+{
+    window.location.href="Williams.html";
+}
+function openKickSauber()
+{
+    window.location.href="KickSauber.html";
+}
